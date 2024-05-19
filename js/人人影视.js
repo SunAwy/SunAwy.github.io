d1d96635c8fa80f86dc66c08ff1f6b1befe3a634
SunAwy <81571381+SunAwy@users.noreply.github.com>
2024-05-19 14:10:36 +0800

    update 人人影视.js

--- js/人人影视.js
+++ js/人人影视.js
@@ -5,7 +5,7 @@ var rule = Object.assign(muban.首图2,{
     url:'/rrtop/fyclass/page/fypage.html',
     searchUrl:'/rrcz.html?wd=**',
     class_parse:'.stui-header__menu li;a&&Text;a&&href;.*/(.*?).html',
-    tab_order:['高清①'],
+    tab_order:['蓝光,高清②,高清①'],
     // cate_exclude:'解说',
     play_parse:true,
     lazy:'',
