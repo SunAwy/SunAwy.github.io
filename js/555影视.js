ca69fa83956624a638bf5cba50b7b3958cedac77
SunAwy <81571381+SunAwy@users.noreply.github.com>
2024-07-22 17:42:36 +0800

    update 555影视.js

--- js/555影视.js
+++ js/555影视.js
@@ -3,7 +3,7 @@ title:'555影视',
 // host:'https://www.555dy.app',
 // host:'https://www.555yy2.com/',
 // host:'https://www.555yy1.com/',
-host:'https://www.555pian.com',
+host:'https://55wum.com/',
 headers:{//网站的请求头,完整支持所有的,常带ua和cookies
     'User-Agent':'MOBILE_UA',
     "Cookie": "searchneed=ok"
